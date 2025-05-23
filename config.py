16710b801fe1e7d94ee43c995df6b1e4f589c4de
dbay-prog <sayaboge@gmail.com>
2025-05-22 20:24:44 -0500

    Cambio de url

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 
 # URL = "http://localhost:8080/predict/"
-URL = "https://reroes-ml01.onrender.com/predict/"
+# URL = "https://reroes-ml01.onrender.com/predict/"
+URL = "https://model-flask-docker-9lue.onrender.com/predict/"
